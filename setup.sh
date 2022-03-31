7bbed16d12a6b5945abc99a52838e4e4c68e2c2e
ggonzalv <galo.gonzalvo@gmail.com>
2022-03-31 19:08:00 +0200

    instructions to do the setup for sesion2 notebook

---
+++ setup.sh
@@ -0,0 +1,7 @@
+conda create -n tf tensorflow
+conda activate tf
+conda install -c anaconda jupyter
+conda install -c anaconda nltk
+conda install -c anaconda pandas 
+conda install -c anaconda matplotlib
+pip install numpy==1.19.5
